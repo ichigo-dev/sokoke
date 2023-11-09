8d1a86584aaacafd367c7c49ca92a77b43bd17ef
ichigo <dev.honda.ichigo@gmail.com>
2023-11-09 09:15:45 +0900

    feat: Add application core

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,104 @@
 /// # Sokoke
 ///
 /// Web application framework for Rust.
+///
+///
+/// ## Usage
+///
+/// ```rust
+/// use sokoke::App;
+///
+/// fn main()
+/// {
+///    let app = App::new();
+///    app.run();
+/// }
+/// ```
 //------------------------------------------------------------------------------
 
-fn sokoke()
+use std::env;
+
+use tokio::runtime::Builder;
+use tokio::net::TcpListener;
+
+
+//------------------------------------------------------------------------------
+/// Application
+//------------------------------------------------------------------------------
+pub struct App
 {
-    println!("Hello, sokoke");
+}
+
+impl App
+{
+    //--------------------------------------------------------------------------
+    /// Creates a new Sokoke application.
+    //--------------------------------------------------------------------------
+    pub fn new() -> Self
+    {
+        Self {}
+    }
+
+    //--------------------------------------------------------------------------
+    /// Returns the version of the application.
+    //--------------------------------------------------------------------------
+    pub fn version( &self ) -> String
+    {
+        env::var("CARGO_PKG_VERSION")
+            .unwrap_or_else(|_| String::from("unknown"))
+    }
+
+    //--------------------------------------------------------------------------
+    /// Runs the application.
+    //--------------------------------------------------------------------------
+    pub fn run( &self )
+    {
+        // Creates a TCP listener which will listen for incoming connections.
+
+        // Creates a multi-threaded Tokio runtime.
+        let runtime = Builder::new_multi_thread()
+            .enable_all()
+            .worker_threads(4)
+            .build()
+            .unwrap();
+        runtime.block_on(async
+        {
+            println!("sokoke: {}", self.version());
+
+            // Starts listening for incoming connections.
+            let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+            self.serve(listener).await;
+        });
+    }
+
+    //--------------------------------------------------------------------------
+    /// Serves the service with the given listener.
+    //--------------------------------------------------------------------------
+    async fn serve( &self, tcp_listener: TcpListener )
+    {
+        loop
+        {
+            let (_stream, _remote_addr) = tcp_listener.accept().await.unwrap();
+        }
+    }
+}
+
+
+//------------------------------------------------------------------------------
+/// Tests
+//------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    //--------------------------------------------------------------------------
+    /// Tests the application.
+    //--------------------------------------------------------------------------
+    #[test]
+    fn test_app()
+    {
+        let app = App::new();
+        app.run();
+    }
 }
